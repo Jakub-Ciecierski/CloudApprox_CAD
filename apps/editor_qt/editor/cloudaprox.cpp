521355b7611039df6c8b7115f8f479b4eb8eed5c
lukaszw896 <lukaszw896@gmail.com>
2016-03-14 23:23:39 +0100

    prototype of bruteforce cloud aproximation function

---
+++ apps/editor_qt/editor/cloudaprox.cpp
@@ -0,0 +1,6 @@
+#include "cloudaprox.h"
+
+CloudAprox::CloudAprox()
+{
+
+}
---
+++ apps/editor_qt/editor/cloudaprox.h
@@ -0,0 +1,11 @@
+#ifndef CLOUDAPROX_H
+#define CLOUDAPROX_H
+
+
+class CloudAprox
+{
+public:
+    CloudAprox();
+};
+
+#endif // CLOUDAPROX_H
